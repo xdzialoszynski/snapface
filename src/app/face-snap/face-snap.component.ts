884ed79de657e32a3eaf29363e68e89664ebba79
Xavier Dzialoszynski <xavier.dzialoszynski@braincube.com>
2024-04-21 14:02:31 +0200

    chapitre2-1

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit{
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps! :number;
+  imageUrl! :string;
+
+  ngOnInit(): void {
+    this.title = "Cesar";
+    this.description = "mon samiaou";
+    this.createdDate = new Date();
+    this.snaps = 0;
+    this.imageUrl="https://images.pexels.com/photos/1049764/pexels-photo-1049764.jpeg?auto=compress&cs=tinysrgb&w=600";
+  }
+}
