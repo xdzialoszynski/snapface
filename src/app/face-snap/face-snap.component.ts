1af1946d21023b174619e3efc3aeb2df27d75de9
Xavier Dzialoszynski <xavier.dzialoszynski@braincube.com>
2024-04-27 12:32:40 +0200

    add snapFaceSnapById and use it

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,21 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
-
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
-  styleUrl: './face-snap.component.scss'
+  styleUrl: './face-snap.component.scss',
 })
 export class FaceSnapComponent {
- @Input() faceSnap! :FaceSnap;
-  
-  
+  constructor(private faceSnapsService: FaceSnapsService) {}
+  @Input() faceSnap!: FaceSnap;
+
   onAddSnap() {
-    if (this.faceSnap.snaps > 0) {
-      this.faceSnap.snaps--;
-    } else {
-      this.faceSnap.snaps++;
-    }
+    this.faceSnapsService.snapFaceSnapById(this.faceSnap.id);
+    // if (this.faceSnap.snaps > 0) {
+    //   this.faceSnap.snaps--;
+    // } else {
+    //   this.faceSnap.snaps++;
+    // }
   }
 }
---
+++ src/app/services/face-snaps.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Injectable({ providedIn: 'root' })
+export class FaceSnapsService {
+  faceSnaps: FaceSnap[] = [
+    {
+      id: 1,
+      createdDate: new Date(),
+      description: 'mon samiaou',
+      imageUrl:
+        'https://images.pexels.com/photos/1049764/pexels-photo-1049764.jpeg?auto=compress&cs=tinysrgb&w=600',
+      snaps: 7,
+      title: 'Césari',
+      location: "l'épanlerie",
+    },
+    {
+      id: 2,
+      createdDate: new Date(Date.parse('01 Jan 2021 00:00:00 GMT')),
+      description: 'le sa du sa',
+      imageUrl:
+        'https://images.pexels.com/photos/1049764/pexels-photo-1049764.jpeg?auto=compress&cs=tinysrgb&w=600',
+      snaps: 0,
+      title: 'Jules',
+      location: 'En Auvergne',
+    },
+    {
+      id: 3,
+      createdDate: new Date(Date.parse('12 Oct 1973 00:00:00 GMT')),
+      description: 'je suis le miaou du samiaou',
+      imageUrl:
+        'https://images.pexels.com/photos/1049764/pexels-photo-1049764.jpeg?auto=compress&cs=tinysrgb&w=600',
+      snaps: 0,
+      title: 'Xavier',
+    },
+  ];
+
+  getAllFaceSnaps(): FaceSnap[] {
+    return this.faceSnaps;
+  }
+
+  snapFaceSnapById(id: number) {
+    const faceSnap = this.faceSnaps.at(id - 1);
+    if (faceSnap) {
+      if (faceSnap.snaps > 0) {
+        faceSnap.snaps--;
+      } else {
+        faceSnap.snaps++;
+      }
+    }
+  }
+}
