0a4b055cbc2d8c1a596cf67fb9896ef5eba76da1
Xavier Dzialoszynski <xavier.dzialoszynski@braincube.com>
2024-04-27 15:17:43 +0200

    add button and use Router

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landingpage',
   templateUrl: './landingpage.component.html',
-  styleUrl: './landingpage.component.scss'
+  styleUrl: './landingpage.component.scss',
 })
 export class LandingpageComponent {
+  constructor(private router: Router) {}
 
+  onGoToSnapFace(): void {
+    this.router.navigateByUrl('facesnaps');
+    throw new Error('Method not implemented.');
+  }
 }
